873db32841ac93940a1d7734aed1f3aec696e8e4
joshuavaslie2021 <joshuavaslie2021@u.northwestern.edu>
2021-02-15 14:18:25 -0600

    initial commit of homework 18

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+// Defining the Cashe for the Logic and JS.
+var CACHE_NAME = "local-cache-v1"
+// Defining the Cache for the data
+
+// const DATA_CACHE_NAME = 
+//Installing the service workers to your application
+
+    // Perform Install Apps
+
+// Cacheing requests to any route that uses the "/api/"
