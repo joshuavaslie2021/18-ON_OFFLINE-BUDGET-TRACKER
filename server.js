ee9f10f4984a16ed61a1f4fad3ffe3a9ca45cdd4
Joshua Vaslie <joshuavaslie2021@u.northwestern.edu>
2021-02-17 00:27:20 -0600

    revision and update of all dependencies

--- server.js
+++ server.js
@@ -15,15 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/ledger',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  )
+mongoose.connect("mongodb://localhost/ledger", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 // routes
 app.use(require("./routes/api.js"));
 
